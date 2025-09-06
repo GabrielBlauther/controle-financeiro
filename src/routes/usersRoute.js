f54df7481602c7694afa021ff11d9ebab4f5f957
Gabriel <gsblauther@ucs.br>
2025-09-06 10:50:40 -0300

    feat: Controllers.

--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -1,33 +1,9 @@
-import { Router } from "express";
+const express = require("express");
+const router = express.Router();
+const { createUser, allUsers, getById } = require("../controllers/usersController");
 
-const router = Router();
-
-
-let users = [
-  {id: 0, name:"Joao", idade:30},
-  {id: 2, name:"Pedro", idade:30},
-  {id: 3, name:"Maria", idade:30},
-]
-
-router.get('/users', (req,res) => {
-  res.json(users)
-  if(req.query.name){
-    res.json(users.filter((r) => r.name.includes(req.query.name)))
-  } else{
-    res.json(users)
-  }
-})
-
-router.get('/users/:id', (req, res) =>{
-  let user = users.find(r => r.id === parseInt(req.params.id))
-  res.json(user)
-})
-
-router.post('/users', (req, res) => {
-  users.push(req.body)//para adicionar os usuarios
-  res.status(201).send()
-})
-
-
+router.get("/", allUsers);
+router.get("/:id", getById);
+router.post("/", createUser);
 
 module.exports = router
